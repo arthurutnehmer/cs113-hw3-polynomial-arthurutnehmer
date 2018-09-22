c15ab258eb45fff912976e807fa6f64c3ddb5b0b
Arthur Utnehmer <arthurutnehmer@gmail.com>
2018-09-21 23:10:43 -0700

    Fixed menue readability

--- test/edu/miracosta/cs113/MainMenu.java
+++ test/edu/miracosta/cs113/MainMenu.java
@@ -2,7 +2,7 @@ package edu.miracosta.cs113;
 import javafx.scene.Scene;
 
 import java.util.Scanner;
-public class MainMenue
+public class MainMenu
 {
     public static void main(String[]args)
     {
@@ -11,7 +11,7 @@ public class MainMenue
         Driver driver = new Driver();
         System.out.println("Welcome to Polynomial adder 1.0");
         System.out.println("select a letter to perform an action");
-        System.out.println("C.) Create polynomial 1.");
+        System.out.println("C ) Create polynomial 1.");
         String selection = keyboard.nextLine();
         selection.toUpperCase();
         while (alive = true)
@@ -21,15 +21,19 @@ public class MainMenue
                 System.out.println("********Please enter your polynomial as followed:********");
                 System.out.println("2x^2 +2x^2 +3x^2");
                 System.out.println("use + or - in front of each term to indicate its polatity.");
-                System.out.println("+2^0 indicates 2");
+                System.out.println("+2 indicates +2");
                 System.out.println("Spaces are used in between terms while ^ is used to indicate a power.");
                 System.out.println("********Please enter your polynomial as followed:********");
                 driver.createPolynomial1();
                 driver.enterPolynomial1AllAtOnce(keyboard.nextLine());
+                System.out.println("********Press Enter to continue:********");
+                String crap = keyboard.nextLine();
             }
             else if(selection.toUpperCase().equals("S"))
             {
                 driver.printPolynomial1User();
+                System.out.println("********Press Enter to continue:********");
+                String crap = keyboard.nextLine();
             }
             else if(selection.toUpperCase().equals("CL"))
             {
@@ -41,15 +45,19 @@ public class MainMenue
                 System.out.println("********Please enter your polynomial as followed:********");
                 System.out.println("2x^2 +2x^2 +3x^2");
                 System.out.println("use + or - in front of each term to indicate its polatity.");
-                System.out.println("+2^0 indicates 2");
+                System.out.println("+2 indicates +2");
                 System.out.println("Spaces are used in between terms while ^ is used to indicate a power.");
                 System.out.println("********Please enter your polynomial as followed:********");
                 driver.createPolynomial2();
                 driver.enterPolynomial2AllAtOnce(keyboard.nextLine());
+                System.out.println("********Press Enter to continue:********");
+                String crap = keyboard.nextLine();
             }
             else if(selection.toUpperCase().equals("S2"))
             {
                 driver.printPolynomial2User();
+                System.out.println("********Press Enter to continue:********");
+                String crap = keyboard.nextLine();
             }
             else if(selection.toUpperCase().equals("CL2"))
             {
@@ -59,6 +67,8 @@ public class MainMenue
             else if(selection.toUpperCase().equals("A"))
             {
                 driver.addPolynomialsTogether();
+                System.out.println("********Press Enter to continue:********");
+                String crap = keyboard.nextLine();
             }
             else if(selection.toUpperCase().equals("E"))
             {
@@ -67,14 +77,14 @@ public class MainMenue
                 break;
             }
             System.out.println("-------------------select a letter to perform an action-------------------");
-            System.out.println("  C.) Create a polynomial1.");
-            System.out.println("  S.) See polynomial 1");
-            System.out.println(" CL.) Clear polynomial 1");
-            System.out.println(" C2.) Create a polynomial2.");
-            System.out.println(" S2.) See polynomial 2");
-            System.out.println("CL2.) Clear polynomial 2");
-            System.out.println("  A.) ADD TERMS TOGETHER");
-            System.out.println("  E.) Exit");
+            System.out.println("  C ) Create a polynomial1.");
+            System.out.println("  S ) See polynomial 1");
+            System.out.println(" CL ) Clear polynomial 1");
+            System.out.println(" C2 ) Create a polynomial2.");
+            System.out.println(" S2 ) See polynomial 2");
+            System.out.println("CL2 ) Clear polynomial 2");
+            System.out.println("  A ) ADD TERMS TOGETHER");
+            System.out.println("  E ) Exit");
             System.out.println("-------------------select a letter to perform an action-------------------");
             selection = keyboard.nextLine();
             selection.toUpperCase();
